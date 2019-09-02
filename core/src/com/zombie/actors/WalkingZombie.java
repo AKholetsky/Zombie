c35d3cb53afbac7796bbd8e7c6790a2775d0727e
Anton Kholetsky <anton.kholetskiy@hiqo-solutions.com>
2019-09-02 18:13:05 +0300

    WalkingZombie stub

---
+++ core/src/com/zombie/actors/WalkingZombie.java
@@ -0,0 +1,11 @@
+package com.zombie.actors;
+
+import com.badlogic.gdx.InputAdapter;
+import com.zombie.animation.GameAnimation;
+
+public class WalkingZombie extends InputAdapter {
+
+    private GameAnimation walkingAnimation;
+
+
+}
